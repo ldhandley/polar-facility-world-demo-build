4f64a2b674988b0f69445d1ee6260dcefc829cae
srfoster <lindseyhandley@gmail.com>
2020-11-25 16:53:45 -0800

    created lore

--- lore.rkt
+++ lore.rkt
@@ -3,14 +3,20 @@
 (require polar-facility-world-demo-build
 	 codespells/lore)
 
-(define-rune-collection-lore 
-  #:name "A Cool Name for PolarFacilityWorldDemoBuild"
+(define-authored-work-lore 
+  #:name "Polar Facility"
   #:description 
   @md{
-    A cool intro about PolarFacilityWorldDemoBuild
+    A cold, forbidding place: it is not known which wizard crafted this world, but clearly the work is from the @b{First Epoch} because the facility is tightly bounded. 
+    @(div
+      (alert-primary "In its current form, we estimate this build to be worth about " (b "2 minutes of fun") ". Enjoy the reflections of fire and ice magic on the metallic walls."))
   }
-  #:rune-lores
-  (list)
+  #:rune-collections
+  (list 'hierarchy
+        'spawners
+        'fire-particles
+	'ice-particles
+	'rocks)
   #:preview-image preview.png)
 
 
